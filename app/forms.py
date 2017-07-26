415439b7c9ee29803026b02a79669fbf9556e513
Nikolay Volosatov <bamx23@yandex-team.ru>
2017-07-26 17:19:02 +0300

    Fix encoding problems

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-  utils.py
+  forms.py
 
   This file is a part of the AppMetrica.
 
-  Copyright © 2017 YANDEX
+  Copyright 2017 YANDEX
 
   You may not use this file except in compliance with the License.
   You may obtain a copy of the License at https://yandex.com/legal/metrica_termsofuse/
--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@
 
   This file is a part of the AppMetrica.
 
-  Copyright © 2017 YANDEX
+  Copyright 2017 YANDEX
 
   You may not use this file except in compliance with the License.
   You may obtain a copy of the License at https://yandex.com/legal/metrica_termsofuse/
